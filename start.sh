154e8a9f896b9445d5a048b010fe7a9f14090270
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-09-11 20:15:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot JishuDeveloper/Ultra-Forward-Bot
+  git clone https://github.com/TELLYHUBCLOUD/Ultra-Forward-Bot TELLYHUBCLOUD/Ultra-Forward-Bot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
+  git clone https://github.com/TELLYHUBCLOUD/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
 fi
 cd JishuDeveloper/Ultra-Forward-Bot
 pip3 install -U -r requirements.txt
