58b12a282c1774d57c7ae7d8b3ffb71ef379b411
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-09-11 20:30:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/TELLYHUBCLOUD/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
 fi
-cd JishuDeveloper/Ultra-Forward-Bot
+cd TELLYHUBCLOUD/Ultra-Forward-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
